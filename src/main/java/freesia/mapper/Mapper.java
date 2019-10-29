df98b521cdff08ad9d8cf96ae294b6090cfbf4ec
Nefarius <nectarius@outlook.com>
2019-10-30 00:34:39 +0300

    remove unused methods

--- src/main/java/freesia/mapper/Mapper.java
+++ src/main/java/freesia/mapper/Mapper.java
@@ -12,16 +12,17 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Consumer;
 
 public class Mapper {
-    // consider use size instead
-    private AtomicReference<ArrayList<Worker>> workers = new AtomicReference<>();
-    private int expectedCount;
-    private CompletableFuture<Outcome> finalResult;
+
     private final WorkerOperation workerOperation;
     private final DataAggregator dataAggregator;
+
+    private AtomicReference<ArrayList<Worker>> workers = new AtomicReference<>();
+    private int countOfFragments;
     private AtomicInteger count = new AtomicInteger(0);
+
+    private CompletableFuture<Outcome> finalResult;
 
     public static Mapper create(WorkerOperation workerOperation) {
         return create(workerOperation, new DataAggregator() {
@@ -35,7 +36,7 @@ public class Mapper {
     private Mapper(WorkerOperation workerOperation, DataAggregator dataAggregator) {
         this.workerOperation = workerOperation;
         this.dataAggregator = dataAggregator;
-        this.expectedCount = 0;
+        this.countOfFragments = 0;
         this.finalResult = null;
     }
 
@@ -43,35 +44,31 @@ public class Mapper {
         return this.finalResult;
     }
 
-    int getExpectedCount() {
-        return this.expectedCount;
-    }
-
-    AtomicReference<ArrayList<Worker>> getWorkers() {
-        return this.workers;
-    }
-
     private ArrayList<Worker> createWorkers(int num) {
 
         ArrayList<Worker> ws = new ArrayList<Worker>(num);
         for (int i = 0; i < num; i++) {
             ws.add(new Worker(workerOperation, integer -> {
-                if (count.getAndIncrement() + 1 == expectedCount) {
-                    //all data obtained
-                    List<Outcome> collectedData = new ArrayList<>();
-                    for (Worker worker : workers.get()) {
-                        collectedData.add(worker.getOutcome());
-                    }
-                    Outcome result = dataAggregator.aggregateData(collectedData);
-                    workers.set(null);
-                    count.set(0);
-                    finalResult.complete(result);
-                }
+                notifyFromWorker();
             }, i + 1));
             //i+1 consider as id
         }
 
         return ws;
+    }
+
+    private void notifyFromWorker() {
+        if (count.getAndIncrement() + 1 == countOfFragments) {
+            //all data obtained
+            List<Outcome> collectedData = new ArrayList<>();
+            for (Worker worker : workers.get()) {
+                collectedData.add(worker.getOutcome());
+            }
+            Outcome result = dataAggregator.aggregateData(collectedData);
+            workers.set(null);
+            count.set(0);
+            finalResult.complete(result);
+        }
     }
 
     public CompletableFuture<Outcome> doWork(Fragment input, int numOfWorkers) {
@@ -88,8 +85,8 @@ public class Mapper {
     private void divideWork(List<Fragment> dividedInput) {
 
         if (dividedInput != null) {
-            this.expectedCount = dividedInput.size();
-            for (int i = 0; i < this.expectedCount; i++) {
+            this.countOfFragments = dividedInput.size();
+            for (int i = 0; i < this.countOfFragments; i++) {
                 this.workers.get().get(i).setReceivedData(this, dividedInput.get(i));
                 this.workers.get().get(i).start();
                 //this.workers.get(i).run();
